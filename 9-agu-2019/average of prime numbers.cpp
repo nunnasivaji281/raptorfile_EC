4ae50dc70a2e1a35e05a84f3db745a7249bf7909
nunnasivaji281 <53816336+nunnasivaji281@users.noreply.github.com>
2019-08-10 09:23:29 +0530

    Add files via upload

---
+++ 9-agu-2019/average of prime numbers.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? b;
+   ?? c;
+   ?? s;
+   ?? cp;
+   ?? j;
+   ?? i;
+
+   raptor_prompt_variable_zzyz ="enter a number ";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> i;
+   raptor_prompt_variable_zzyz =" enter a number ";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> b;
+   s =0;
+   cp =0;
+   while (1)
+   {
+      c =0;
+      if (i>b)) break;
+      j =1;
+      while (!(j>i))
+      {
+         if (i % j==0)
+         {
+            c =c+1;
+         }
+         else
+         {
+         }
+         j =j+1;
+      }
+      if (c>=2)
+      {
+      }
+      else
+      {
+         cout << i << endl;         s =s+i;
+         cp =cp+1;
+      }
+      i =i+1;
+   }
+   cout << s/cp << endl;
+   return 0;
+}
